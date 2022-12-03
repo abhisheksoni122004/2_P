a92f3da21c6f36f01777a0541796196037859273
abhisheksoni122004 <113802753+abhisheksoni122004@users.noreply.github.com>
2022-12-03 21:29:01 +0530

    Add files via upload

---
+++ time code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int dis,time;
+	float sp;
+	cout<<"Enter the distance: ";
+	cin>>dis;
+	cout<<"Enter the time: ";
+	cin>>time;
+	sp=dis/time;
+	cout<<"the speed is: "<<sp<<endl;
+}
+ 
+  
